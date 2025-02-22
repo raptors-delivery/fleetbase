02180d41077459b15768b72b66a0e64abc57f861
Shady Khalifa <dev+github@shadykhalifa.me>
2025-02-22 16:47:58 +0200

    fix: update PHP version and xdebug configuration - Upgrade PHP from 8.2 to 8.3 and enable embed support. - Adjust xdebug settings to turn off by default.

--- flake.nix
+++ flake.nix
@@ -5,41 +5,67 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        myphp = pkgs.php82;
+        stdenv = pkgs.stdenv;
+        myphp = pkgs.php83.override {
+          embedSupport = true;
+          ztsSupport = true;
+          staticSupport = stdenv.hostPlatform.isDarwin;
+          zendSignalsSupport = false;
+          zendMaxExecutionTimersSupport = stdenv.hostPlatform.isLinux;
+        };
         geos = pkgs.callPackage ./nix/php/geos.nix {
           inherit myphp;
         };
         php = myphp.buildEnv {
-          extensions = ({ enabled, all }: enabled ++ (with all; [
-            # Add More PHP Extensions Here
-            xdebug
-            gd
-            redis
-            pdo
-            pdo_mysql
-            gmp
-            imagick
-            bcmath
-            opcache
-            intl
-            memcached
-            sockets
-            pcntl
-            zip
-            apcu
-            geos
-          ]));
+          extensions = (
+            { enabled, all }:
+            enabled
+            ++ (with all; [
+              # Add More PHP Extensions Here
+              xdebug
+              gd
+              redis
+              pdo
+              pdo_mysql
+              gmp
+              imagick
+              bcmath
+              opcache
+              intl
+              memcached
+              sockets
+              pcntl
+              zip
+              apcu
+              geos
+            ])
+          );
           extraConfig = ''
             date.timezone = "UTC"
-            xdebug.mode=debug
+            # turn off xdebug by default
+            xdebug.mode=off
+            #xdebug.mode=debug,develop
+            #xdebug.start_with_request=yes
+            #xdebug.start_upon_error=yes
+            #xdebug.idekey=VSCODE
           '';
+        };
+
+        frankenphp = pkgs.frankenphp.override {
+          inherit php;
         };
       in
       {
@@ -53,6 +79,7 @@
             pkgs.pnpm
 
             php
+            frankenphp
             php.packages.composer
             php.packages.php-cs-fixer
             php.packages.phpstan
@@ -62,5 +89,6 @@
           ];
           packages = [ ];
         };
-      });
+      }
+    );
 }
